2f219261f7b38d7fe0ca47b6c6d785142fec965d
Ubaldo Cotta <ucotta@me.com>
2016-10-25 17:34:57 +0300

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "Perfect-Session",
-    targets: [],
-    dependencies: [
-//		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0)
-    ],
-    exclude: []
+	name: "SessionMemory",
+	targets: [],
+	dependencies: {
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git"), majorVersion: 2, minor: 0)
+	exclude: []
 )
+
