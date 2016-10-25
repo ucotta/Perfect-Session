b51fb2e018a9feddd99e4dee41b05fcaf0de33ef
Ubaldo Cotta <ucotta@me.com>
2016-10-25 18:04:49 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ let package = Package(
 	name: "SessionMemory",
 	targets: [],
 	dependencies: {
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git"), majorVersion: 2, minor: 0)
+        	.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0)
+	},
 	exclude: []
 )
 
